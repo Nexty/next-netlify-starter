20184d11cf84b372d41a77c08c9080d41307ae8f
Andrew Barr <abarr.rac@gmail.com>
2022-12-10 22:50:33 +0000

    Add contentful

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,11 @@
 import Head from 'next/head'
+
+
+import { fetchEntries } from '@utils/contentfulPosts'
+
 import Header from '@components/Header'
 import Footer from '@components/Footer'
+import Post from '@components/Post'
 
 export default function Home() {
   return (
@@ -14,10 +19,29 @@ export default function Home() {
         <Header title="Welcome to my app!" />
         <p className="description">
           Get started by editing <code>pages/index.js</code>
-        </p>
+              </p>
+
+              <div className="posts">
+                  {posts.map((p) => {
+                      return <Post key={p.date} date={p.date} image={p.image.fields} title={p.title} />
+                  })}
+              </div>
       </main>
 
       <Footer />
     </div>
   )
+}
+
+export async function getStaticProps() {
+    const res = await fetchEntries()
+    const posts = await res.map((p) => {
+        return p.fields
+    })
+
+    return {
+        props: {
+            posts,
+        },
+    }
 }
